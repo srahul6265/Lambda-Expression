f0c6577dedec348b88d9468a5d2ac079368f2582
srahul6265 <rahulsbhutekar9794@gmail.com>
2023-02-12 15:38:59 +0530

    Filr Upload

---
+++ src/lambda_Expression/Test.java
@@ -0,0 +1,14 @@
+package lambda_Expression;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println("Start");
+	
+	//(int a , int b )->System.out.println(a+b); 
+		
+	
+	System.out.println(" Hello git Bhayyya");
+	
+	
+}
+}
