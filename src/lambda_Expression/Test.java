f35bb67bbeb75deb6df7a14e52f9c9dee525f288
srahul6265 <rahulsbhutekar9794@gmail.com>
2023-02-12 16:06:25 +0530

    udate again

--- src/lambda_Expression/Test.java
+++ src/lambda_Expression/Test.java
@@ -10,5 +10,8 @@ public static void main(String[] args) {
 	System.out.println(" Hello git Bhayyya");
 	System.out.println("Hello agin git bhayya");
 	System.out.printn("Hello Git bhayya apka clone kardiya hai");
+	System.out.printn("Hello Git bhayya apka phirse clone kardiya hai");
+
+
 }
 }
